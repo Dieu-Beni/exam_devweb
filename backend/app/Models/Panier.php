e8de9f66ba0ca2086799e797c74f96ddac8b2f1a
dia junior <abdourahmanedia006@gmail.com>
2025-07-29 21:22:13 +0000

    panier model update

--- backend/app/Models/Panier.php
+++ backend/app/Models/Panier.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use App\Models\Commande;
 use App\Models\Panier_produit;
 
 
@@ -12,15 +13,23 @@ class Panier extends Model
     protected $fillable = ['id_user', 'statut'];
 
 
-    public function utilisateur()
+    public function user()
     {
-        return $this->belongsTo(User::class, 'id_user');
+        return $this->belongsTo(User::class);
     }
+
 
     public function produits()
     {
-        return $this->hasMany(Panier_produit::class, 'id_panier');
+        return $this->belongsToMany(Produit::class, 'panier_produit')
+                    ->withPivot('quantite', 'montant')
+                    ->withTimestamps();
     }
+
+    public function commande()
+{
+    return $this->hasOne(Commande::class, 'id_panier');
+}
 
 
 }
