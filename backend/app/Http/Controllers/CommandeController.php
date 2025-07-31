dc41ee04aed2772f59895bef28c2309ad731d714
dia junior <abdourahmanedia006@gmail.com>
2025-07-31 02:06:58 +0000

    ajout d'un use au niveau de commandecontroller pour le storage

--- backend/app/Http/Controllers/CommandeController.php
+++ backend/app/Http/Controllers/CommandeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Commande;
 use App\Models\Paiement;
+use Illuminate\Support\Facades\Storage;
+
 use App\Models\Facture;
 use Illuminate\Support\Carbon;
 use PDF;
