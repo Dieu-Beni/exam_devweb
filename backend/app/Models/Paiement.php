7d23b893dc44e123dc6068d1cbbb7af7eb0d6469
dia junior <abdourahmanedia006@gmail.com>
2025-08-02 20:22:09 +0000

    update des divers fichiers encore

--- backend/app/Models/Paiement.php
+++ backend/app/Models/Paiement.php
@@ -16,4 +16,17 @@ class Paiement extends Model
         return $this->belongsTo(Commande::class, 'id_commande');
     }
 
+    public static function getByCommande($commandeId)
+    {
+        return self::with('carte')
+            ->where('id_commande', $commandeId)
+            ->first();
+    }
+
+    public function carte()
+    {
+        return $this->hasOne(Carte::class, 'id_paiement');
+    }
+
+
 }
